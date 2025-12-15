109a6bfb221a3b0fcc15409d1d890a8866ebd164
nimrodmiz <nimrodmiz328@gmail.com>
2025-12-15 12:03:43 +0200

    my turn to play

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function greet() {
 }
 
 console.log("Website loaded!");
+
+
+const btn = document.getElementById("try");
+
+btn.addEventListener("mouseover", () => {
+    const screenWidth = window.innerWidth - btn.offsetWidth;
+    const screenHeight = window.innerHeight - btn.offsetHeight;
+
+    const randomX = Math.random() * screenWidth;
+    const randomY = Math.random() * screenHeight;
+
+    btn.style.left = randomX + "px";
+    btn.style.top = randomY + "px";
+});
