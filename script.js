b30d3aed09278fd99c6b68e70f6776e56954d52f
nimrodmiz <nimrodmiz328@gmail.com>
2025-12-15 12:08:02 +0200

    my turn to play

--- script.js
+++ script.js
@@ -5,15 +5,6 @@ function greet() {
 console.log("Website loaded!");
 
 
-const btn = document.getElementById("try");
 
-btn.addEventListener("mouseover", () => {
-    const screenWidth = window.innerWidth - btn.offsetWidth;
-    const screenHeight = window.innerHeight - btn.offsetHeight;
 
-    const randomX = Math.random() * screenWidth;
-    const randomY = Math.random() * screenHeight;
 
-    btn.style.left = randomX + "px";
-    btn.style.top = randomY + "px";
-});
