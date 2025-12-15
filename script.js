3b537307892853103f2bf86c6f31416186e79713
ozbash <ozaq12@gmail.com>
2025-12-15 12:11:22 +0200

    update

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function greet() {
 }
 
 console.log("Website loaded!");
+
+<script>
+    const btn = document.getElementById("moveBtn");
+
+    btn.addEventListener("click", () => {
+        const x = Math.random() * 200 - 100; // ימינה / שמאלה
+    const y = Math.random() * 200 - 100; // למעלה / למטה
+
+    btn.style.transform = `translate(${x}px, ${y}px)`;
+    });
+</script>
